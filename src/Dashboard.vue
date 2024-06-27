d40eb095397d1b94ebe2ba115058e0b6deb54c66
Huang <mains2114@qq.com>
2024-06-27 17:46:52 +0800

    首页仪表盘：改造为组合式api，去除分页，改进生产商过滤和排序

--- src/Dashboard.vue
+++ src/Dashboard.vue
@@ -6,7 +6,6 @@
           <div slot="header">
             <span>库存预警</span>
           </div>
-
           <el-form :inline="true">
             <el-form-item>
               <el-radio-group v-model="lowStorageParams.showAll" @change="getRows">
@@ -14,26 +13,25 @@
                 <el-radio-button label="0">低库存预警</el-radio-button>
               </el-radio-group>
             </el-form-item>
+            <el-form-item>
+              <el-button type="primary" @click="getRows">刷新</el-button>
+            </el-form-item>
           </el-form>
-
-          <el-table :data="tableRows1" :rowClassName="tableRowClassName" border v-loading="loading">
-            <el-table-column prop="product.company.name" label="生产商" min-width="100" sortable :filters="pager1.filters" :filter-method="companyFilter"></el-table-column>
+          <el-table :data="tableRows" :rowClassName="tableRowClassName" height="700" border v-loading="loading">
+            <el-table-column prop="product.company.name" label="生产商" min-width="100" sortable :filters="tableFilters"
+              :filter-method="companyFilter"></el-table-column>
             <el-table-column prop="product.name" label="货品及规格" min-width="160" sortable>
               <template slot-scope="scope">
-                <el-link type="primary" :underline="false"
-                  @click="openProductDialog(scope.row.product)"
-                >{{ scope.row.product.name + ' - ' + scope.row.product.model }}</el-link>
+                <el-link type="primary" :underline="false" @click="openProductDialog(scope.row.product)">
+                  {{ scope.row.product.name + ' - ' + scope.row.product.model }}
+                </el-link>
               </template>
             </el-table-column>
             <el-table-column prop="num" label="数量" min-width="80"></el-table-column>
             <el-table-column prop="warning_num" label="预警值" min-width="80"></el-table-column>
           </el-table>
           <br>
-          <el-pagination layout="total, sizes, prev, pager, next" background
-                   :total="pager1.total"
-                   :page-size.sync="pager1.size"
-                   :current-page.sync="pager1.page"
-                   :page-sizes="[10, 20, 50]">
+          <el-pagination layout="total" background :total="tableRows.length">
           </el-pagination>
         </el-card>
       </el-col>
@@ -43,14 +41,19 @@
           <div slot="header">
             <span>过期预警</span>
           </div>
-
-          <el-table :data="tableRows2" border v-loading="loading2">
+          <el-form :inline="true">
+            <el-form-item>
+              <el-button type="primary" @click="getExpireWarning">刷新</el-button>
+            </el-form-item>
+          </el-form>
+          <el-table :data="nearExpireStock" height="700" border v-loading="loading2"
+            :default-sort="{ prop: 'expire', order: 'ascending' }">
             <el-table-column prop="product.company.name" label="生产商" min-width="100" sortable></el-table-column>
             <el-table-column prop="product.name" label="货品及规格" min-width="160" sortable>
               <template slot-scope="scope">
-                <el-link type="primary" :underline="false"
-                  @click="openProductDialog(scope.row.product)"
-                >{{ scope.row.product.name + ' - ' + scope.row.product.model }}</el-link>
+                <el-link type="primary" :underline="false" @click="openProductDialog(scope.row.product)">
+                  {{ scope.row.product.name + ' - ' + scope.row.product.model }}
+                </el-link>
               </template>
             </el-table-column>
             <el-table-column prop="num" label="数量" min-width="80"></el-table-column>
@@ -64,11 +67,7 @@
             </el-table-column>
           </el-table>
           <br>
-          <el-pagination layout="total, sizes, prev, pager, next" background
-                   :total="pager2.total"
-                   :page-size.sync="pager2.size"
-                   :current-page.sync="pager2.page"
-                   :page-sizes="[10, 20, 50]">
+          <el-pagination layout="total" background :total="nearExpireStock.length">
           </el-pagination>
         </el-card>
       </el-col>
@@ -77,125 +76,102 @@
     </el-row>
   </div>
 </template>
-
-<script>
-  export default {
-    name: "Dashboard",
-    data() {
-      return {
-        loading: false,
-        loading2: false,
-        lowStorageStock:[],
-        lowStorageParams: {
-          showAll: 1,
-        },
-        nearExpireStock: [],
-        pager1: {
-          total: 0,
-          size: 10,
-          page: 1,
-          filters: [],
-        },
-        pager2: {
-          total: 0,
-          size: 10,
-          page: 1,
-        },
-        urls: {
-          getInventoryWarning: this.url('/api/getInventoryWarning'),
-          getExpireWarning: this.url('/api/getExpireWarning'),
-          lockStorage: this.url('/api/lockStorage'),
-        }
-      }
-    },
-    computed: {
-      tableRows1() {
-        let start = (this.pager1.page - 1) * this.pager1.size;
-        let end = start + this.pager1.size;
-        return this.lowStorageStock.slice(start, end);
-      },
-      tableRows2() {
-        let start = (this.pager2.page - 1) * this.pager2.size;
-        let end = start + this.pager2.size;
-        return this.nearExpireStock.slice(start, end);
-      },
-    },
-    methods: {
-      companyFilter(value, row, column) {
-        return value == row.product.company.name;
-      },
-      openProductDialog(product) {
-        this.$refs.refProductInfoDialog.show(product);
-      },
-      getRows() {
-        this.loading = true;
-
-        this.$http.get(this.urls.getInventoryWarning, {
-          params: {
-            ...this.lowStorageParams,
-            sort: 'id',
-            order: 'desc',
-          }
-        }).then(response => {
-          this.lowStorageStock = response.data.rows || [];
-          this.pager1.total = this.lowStorageStock.length;
-
-          let companyArr = this.lowStorageStock.map((el, idx) => {
-            return el.product.company.name;
-          });
-          this.pager1.filters = Array.from(new Set(companyArr), (el, index) => { return { text: el, value: el }; });
-
-          this.loading = false;
-        })
-      },
-      tableRowClassName({row, rowIndex}) {
-        if (row.num - row.warning_num <= 0) {
-          return 'warning-row';
-        }
-        return '';
-      },
-      getRows2() {
-        this.loading2 = true;
-
-        this.$http.get(this.urls.getExpireWarning, {
-          params: {
-            sort: 'id',
-            order: 'desc',
-          }
-        }).then(response => {
-          this.nearExpireStock = response.data.rows || [];
-          this.pager2.total = this.nearExpireStock.length;
-
-          this.loading2 = false;
-        })
-      },
-      lockExpireStock(rowId) {
-        let param = {
-          rowId,
-        };
-
-        this.$http.post(this.urls.lockStorage, param).then(response => {
-          if (response.data.error === 0) {
-            this.$message.success(response.data.msg || '操作成功');
-
-            this.getRows2();
-            return;
-          }
-
-          console.log(response);
-          this.$message.error(response.data.msg || '请求错误');
-        });
-      },
-    },
-    created() {
-      this.getRows();
-      this.getRows2();
-    }
-  }
-</script>
-
 <style>
 .el-table .warning-row {
   background: oldlace;
 }
 </style>
+<script setup>
+import { ref, onMounted, getCurrentInstance } from 'vue'
+
+const $message = getCurrentInstance().proxy.$message;
+const $http = getCurrentInstance().proxy.$http;
+
+const refProductInfoDialog = ref(null)
+const loading = ref(false)
+const tableRows = ref([])
+const tableFilters = ref([])
+const lowStorageParams = ref({
+  showAll: 1,
+})
+const loading2 = ref(false)
+const nearExpireStock = ref([])
+const urls = {
+  getInventoryWarning: '/api/getInventoryWarning',
+  getExpireWarning: '/api/getExpireWarning',
+  lockStorage: '/api/lockStorage',
+}
+
+function openProductDialog(product) {
+  refProductInfoDialog.value.show(product);
+}
+// 获取库存预警数据
+function getRows() {
+  loading.value = true;
+  $http.get(urls.getInventoryWarning, {
+    params: {
+      ...lowStorageParams.value,
+      sort: 'id',
+      order: 'desc',
+    }
+  }).then(response => {
+    tableRows.value.splice(0, tableRows.value.length, ...(response.data.rows || []));
+
+    let companyArr = tableRows.value.map((el) => {
+      return el.product.company.name;
+    });
+    tableFilters.value.splice(0, tableFilters.value.length,
+      ...Array.from(new Set(companyArr), (el) => { return { text: el, value: el }; })
+    );
+  }).finally(() => {
+    loading.value = false;
+  })
+}
+// 筛选公司名称
+function companyFilter(value, row, column) {
+  return value == row.product.company.name;
+}
+// 库存数量小于预警值时，高亮显示
+function tableRowClassName({ row }) {
+  if (row.num - row.warning_num <= 0) {
+    return 'warning-row';
+  }
+  return '';
+}
+// 获取过期预警数据
+function getExpireWarning() {
+  loading2.value = true;
+  $http.get(urls.getExpireWarning, {
+    params: {
+      sort: 'id',
+      order: 'desc',
+    }
+  }).then(response => {
+    nearExpireStock.value.splice(0, nearExpireStock.value.length, ...(response.data.rows || []));
+  }).finally(() => {
+    loading2.value = false;
+  })
+}
+// 锁定过期库存
+function lockExpireStock(rowId) {
+  let param = {
+    rowId,
+  };
+
+  $http.post(urls.lockStorage, param).then(response => {
+    if (response.data.error === 0) {
+      $message.success(response.data.msg || '操作成功');
+      getExpireWarning();
+      return;
+    }
+
+    console.log(response);
+    $message.error(response.data.msg || '请求错误');
+  });
+}
+
+onMounted(() => {
+  getRows();
+  getExpireWarning();
+})
+</script>
